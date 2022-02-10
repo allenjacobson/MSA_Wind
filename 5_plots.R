7648df8095a05b6a9144e8e46816557c6f5e0a69
Lianne Allen-Jacobson <lianne.allen-jacobson@noaa.gov>
2022-02-10 14:08:02 -0500

    Cleaned up plots, removed old labs

--- 5_plots.R
+++ 5_plots.R
@@ -4,18 +4,17 @@ library(sf)
 library(dplyr)
 library(ggplot2)
 library(cowplot)
+library(stringr)
+
 
 ##############################
 # Functions
 
-MinMeanSEMMax <- function(x) {
-  v <- c(min(x), mean(x) - sd(x)/sqrt(length(x)), mean(x), mean(x) + sd(x)/sqrt(length(x)), max(x))
-  names(v) <- c("ymin", "lower", "middle", "upper", "ymax")
-  v
-}
-
-##############################
-# Functions
+# MinMeanSEMMax <- function(x) {
+#   v <- c(min(x), mean(x) - sd(x)/sqrt(length(x)), mean(x), mean(x) + sd(x)/sqrt(length(x)), max(x))
+#   names(v) <- c("ymin", "lower", "middle", "upper", "ymax")
+#   v
+# }
 
 ##############################
 # Set directories
@@ -71,7 +70,7 @@ dt_area[, log_ratio := log(vtrb_area/sfch_area)][
   , diff_over_under := ifelse(diff > 0, "over", ifelse(log_ratio < 0, "under", "equal"))]
 
 dt_area$percentile <- factor(dt_area$percentile,
-                             levels = c("25th", "50th", "75th", "100th"))
+                             levels = c("100th", "75th", "50th", "25th"))
 
 # prep bias dt
 dt_bias <- as.data.table(sf_bias)
@@ -80,112 +79,72 @@ dt_bias <- dt_bias[type %in% c("false_positive", "false_negative"),
                         .(tripid, percentile, area, type)]
   
 dt_bias$percentile <- factor(dt_bias$percentile,
-                                  levels = c("25th", "50th", "75th", "100th"))
+                             levels = c("100th", "75th", "50th", "25th"))
 
 dt_bias_summary <- dt_bias[, .(total_bias = sum(area)), by = .(percentile, type)]
+
+dt_bias_summary$type2 <- str_replace(dt_bias_summary$type, "_", " ")
 
 
 ##############################
 #plot area diff - over/underestimationggplot(data = dt_bias_size)+
 
-(plot_ratio <- ggplot(data = dt_area) +
-   geom_boxplot(aes(x=percentile, y=log_ratio, fill=percentile)))
+# (plot_ratio <- ggplot(data = dt_area) +
+#    geom_boxplot(aes(x=percentile, y=log_ratio, fill=percentile)))
    
-
-(plot_log_ratio <- ggplot(data = dt_area) +
-  stat_summary(aes(x = percentile, y = log_ratio, fill = percentile),
-               fun.data=MinMeanSEMMax, geom="boxplot", show.legend = FALSE) + 
-  theme_classic(base_size = 16)+
-  geom_hline(yintercept=0, linetype="dashed", color = "red")+
-  xlab("Percentile"))
 
 (plot_hist_log <- ggplot(dt_area) +  
   geom_histogram( aes(x = log_ratio, fill = log_over_under),
                   position = "stack", boundary=1, bins = 15)+
   scale_fill_manual(values=c("black", "red"))+
-  facet_wrap(~ percentile, nrow = 4))
+  facet_wrap(~ percentile, nrow = 1)+
+  coord_flip()+
+  ylab("Count: number of trips")+
+  xlab("Size bias in VTR Footprint*")+
+  labs(title = "Size bias",
+       subtitle = "Does VTR footprint over or underestimate active fishing footprint?",
+       caption = expression(paste("*Bias = Ln",
+                                   bgroup("(",
+                                          over(Area[VTR],Area[ActiveFishing]),
+                                          ")"))), 
+       fill = NULL))
 
-(plot_hist_diff <- ggplot(dt_area) +  
-    geom_histogram( aes(x = diff, fill = diff_over_under),
-                    position = "stack", boundary=0, bins = 15)+
-    scale_fill_manual(values=c("black", "red"))+
-    facet_wrap(~ percentile, nrow = 4))
+width = 8
+height = width*.618
+
+ggsave(filename = paste0(dir_output, "/plot_hist_log.png"),
+       plot = plot_hist_log, width = width, height = height)
+
+
+# (plot_hist_diff <- ggplot(dt_area) +  
+#     geom_histogram( aes(x = diff, fill = diff_over_under),
+#                     position = "stack", boundary=0, bins = 15)+
+#     scale_fill_manual(values=c("black", "red"))+
+#     facet_wrap(~ percentile, nrow = 4)+
+#     coord_flip()+
+#     ylab("Count: number of trips")+
+#     xlab("Bias in VTR Footprint*")+
+#     labs(title = "Size bias",
+#          subtitle = "Does VTR footprint over or underestimate active fishing footprint?",
+#          caption = expression(paste("*Bias = ", Area[VTR] - Area[ActiveFishing])), 
+#          fill = NULL))
 
 # plot mismatch
 (plot_mismatch <- ggplot( data = dt_bias_summary)+
-  geom_bar(aes(x = percentile, y = total_bias, fill = type), stat = "identity")+
+  geom_bar(aes(x = percentile, y = total_bias, fill = type2), stat = "identity")+
   scale_fill_manual(values=c("red", "black"))+
   #scale_y_log10()+
   xlab("Percentile")+
-  ylab("False Area")+
-  labs(title = "False positives and negatives: cumulative area"))
-  
-##############################
-# old plots
+  ylab("Spatial mismatch*")+
+  labs(title = "Spatial mismatch",
+       subtitle ="Can we minimize false positives and negatives?",
+       caption = "*cumulative area across all trips",
+       fill = NULL))
 
-dtOverUnderEstimation <-  data.table(percentile = as.factor(dtSummaryByTripID[, percentile]),
-                                     areaDiffVTRBminusCH = dtSummaryByTripID$areaDiffVTRBminusCH,
-                                     areaVTR = dtSummaryByTripID$areaVTR,
-                                     areaSF = dtSummaryByTripID$areaSF)
 
-dtOverUnderEstimation[, lnArea_VTR_SF := log(dtOverUnderEstimation$areaVTR/dtOverUnderEstimation$areaSF)]
+width = 8
+height = width*.618
 
-vtrbColors <- c("#388E3C", "#689F38", "#AFB42B", "#FDD835") # 25, 50, 75, 100
-
-dtOverUnderEstimation$percentile <- factor(dtOverUnderEstimation$percentile,       # Change ordering manually
-                                           levels = c("25th", "50th", "75th", "100th"))
-
-plotOverUnderEstimation <- ggplot(data = dtOverUnderEstimation,
-                                  aes(x = percentile, y = lnArea_VTR_SF,
-                                      fill = percentile, na.rm = TRUE))+
-  #geom_boxplot()+
-  geom_violin()+
-  geom_dotplot(binaxis = "y",
-               stackdir = "center",
-               dotsize = 0.25,
-               alpha = 0.5,
-               method = "histodot") +
-  scale_fill_manual(values=vtrbColors)+
-  xlab("Percentile")+
-  ylab("Log Ratio: log10(VTRB/SFCH)")+
-  labs(title = "Which VTRB percentile matches SFCH by area?")
-
-ggsave("output/plotOverUnderEstimation.png", plot = plotOverUnderEstimation)
-
-##############################
-
-summaryOverUnder <-  dtOverUnderEstimation[, .( n = length(lnArea_VTR_SF),
-                                                mean = mean(lnArea_VTR_SF),
-                                                min = min(lnArea_VTR_SF),
-                                                max = max(lnArea_VTR_SF),
-                                                se = sd(lnArea_VTR_SF)/length(lnArea_VTR_SF)),
-                                           by = percentile]
-
-g1 <- ggplot(data = summaryOverUnder) +
-  geom_bar(aes(x=percentile, y=mean),
-           stat="identity") +
-  geom_errorbar(aes(x=percentile, ymin=mean-se, ymax=mean+se),
-                width=0.4, size=1.3)
-
-##############################
-#Plot areas diff as histogram  
-dtOverUnderEstimation[, overUnder :=
-                        ifelse(dtOverUnderEstimation$areaDiffVTRBminusCH >0, "Over",
-                               ifelse(dtOverUnderEstimation$areaDiffVTRBminusCH < 0,
-                                      "Under", "No Difference"))]
-
-dtOverUnderEstimation[, overUnder := as.factor(overUnder)]
-
-dtOverUnderEstimation[, areaDiffVTRBminusCHHectares := areaDiffVTRBminusCH*0.0001]
-
-histColors <- c("#FDD835", "#388E3C")
-dtOverUnderEstimation[, .N, by= .(percentile, overUnder)]
-
-histOverUnder<- ggplot(dtOverUnderEstimation, 
-                       aes(x = areaDiffVTRBminusCHHectares, fill = overUnder)) + 
-  geom_histogram(position = "stack", boundary=0, bins = 15)+
-  scale_fill_manual(values=histColors)+
-  facet_wrap(~ percentile, nrow = 4)+
-  scale_y_log10()
-
+ggsave(filename = paste0(dir_output, "/plot_mismatch.png"),
+       plot = plot_mismatch, width = width, height = height)
 
