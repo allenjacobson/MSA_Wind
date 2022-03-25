cc04170c7defd52701c86b5c689bab7ffea400fe
Lianne Allen-Jacobson <lianne.allen-jacobson@noaa.gov>
2022-03-25 12:05:38 -0400

    created and exported cumulative rasters with new cell values by revenue - under for different scenarios

--- 2c_revenue_to_rasters_by_imgid_percentile.R
+++ 2c_revenue_to_rasters_by_imgid_percentile.R
@@ -9,23 +9,6 @@ library(terra)
 
 ##############################
 # Functions
-list_rasters <- dt_25$paths
-this_trip <- unique_trips[[2]]
-
-paths_to_mosaic <- function(list_rasters, this_trip){
-  length <- length(list_rasters)
-  if(length == 1){
-    mosaic <- rast(list_rasters)
-    return(mosaic)
-  } else if (length > 1){
-    rasters <- lapply(X = list_rasters, FUN = rast)
-    mosaic <- do.call(mosaic, args = c(rasters, fun = "max"))
-    return(mosaic)
-  } else{ 
-    this_warning <- paste("Error with ", this_trip, sep = " ")
-    warning(this_warning)
-  }
-}
 
 ##############################
 # Set directories
@@ -43,12 +26,29 @@ dir_data <- paste0(path_base, "Data/", repository)
 ##############################
 # Pull in data
 dt_paths_vtrb_split<- readRDS(paste0(dir_output, "/dt_paths_vtrb_split_by_percentile.rds"))
-dt_revenue_matched<- readRDS(paste0(dir_output, "/dt_revenue.rds"))
+dt_revenue<- setDT(readRDS(paste0(dir_output, "/dt_revenue_matched.rds")))
 
+# Data Prep
+dt_revenue_matched <- dt_revenue[imgid_chr %in% dt_paths_vtrb_split$imgid]
+dt_paths_vtrb_split_matched <- dt_paths_vtrb_split[imgid %in% dt_revenue_matched$imgid_chr]
+
+length(unique(dt_revenue_matched$imgid_chr)) == length(unique(dt_paths_vtrb_split_matched$imgid))
+
+# join revenue and path tables
+# this should happen earlier - move to data prep, and move data pull earlier
+setkey(dt_revenue_matched, imgid_chr)
+setkey(dt_paths_vtrb_split_matched, imgid)
+dt <- dt_paths_vtrb_split_matched[dt_revenue_matched, nomatch = 0]
+
+dt[, revenue_per_cell_90 := value*value_gdp]
+dt[, revenue_per_cell_75 := value_lower_3*value_gdp]
+dt[, revenue_per_cell_50 := value_lower_2*value_gdp]
+dt[, revenue_per_cell_25 := value_lower_1*value_gdp]
+
+saveRDS(dt, paste0(dir_output, "/dt_paths_vtrb_split_matched_revenue") )
 ##############################
 # Raster mosaics by percentiles
-unique_trips <- unique(dt_paths_vtrb_split$trip_area)
-this_trip <- unique_trips[[2]]
+unique_trips <- unique(dt$imgid)
 
 # create empty data.table to add file paths
 #dtCumulativeRasterPaths <- data.table()
@@ -56,46 +56,62 @@ dt_paths_vtrb_revenue <- data.table()
 
 
 for (this_trip in unique_trips) {
-  dt_25 <- dt_paths_vtrb_split[trip_area==this_trip & percentile=="25th"]
-  dt_50 <- dt_paths_vtrb_split[trip_area==this_trip & percentile=="50th"]
-  dt_75 <- dt_paths_vtrb_split[trip_area==this_trip & percentile=="75th"]
-  dt_100 <- dt_paths_vtrb_split[trip_area==this_trip & percentile=="100th"]
-  # create mosaic for each percentile - these are non-cumulative
-  mosaic_25<- paths_to_mosaic(dt_25$paths, this_trip)
-  mosaic_50<- paths_to_mosaic(dt_50$paths, this_trip)
-  mosaic_75<- paths_to_mosaic(dt_75$paths, this_trip)
-  mosaic_100<- paths_to_mosaic(dt_100$paths, this_trip)
-  # create cumulative mosaic for each percentile
-  cumulative_25 <- mosaic_25
-  cumulative_50 <- mosaic(mosaic_50, cumulative_25, fun = "max") 
-  cumulative_75 <- mosaic(mosaic_75, cumulative_50, fun = "max")
-  cumulative_100 <- mosaic(mosaic_100, cumulative_75, fun = "max")
+  trip_data <- dt[imgid==this_trip]
+  # create new rasters for each percentile - these are non-cumulative
+  top4_25th <- ifel( rast(trip_data[percentile=="25th", paths]) == 1,
+                     trip_data[percentile=="25th", revenue_per_cell_90], 0)
+  top4_50th <- ifel( rast(trip_data[percentile=="50th", paths]) == 1,
+                     trip_data[percentile=="50th", revenue_per_cell_90], 0)
+  top4_75th <- ifel( rast(trip_data[percentile=="75th", paths]) == 1,
+                     trip_data[percentile=="75th", revenue_per_cell_90], 0)
+  top4_90th <- ifel( rast(trip_data[percentile=="90th", paths]) == 1,
+                     trip_data[percentile=="90th", revenue_per_cell_90], 0)
+  cumulative_top4 <- mosaic(top4_90th, top4_75th, fun = "sum")
+  cumulative_top4 <- mosaic(cumulative_top4, top4_50th, fun = "sum")
+  cumulative_top4 <- mosaic(cumulative_top4, top4_25th, fun = "sum")
+  
+  # Repeat for top 3 confidence levels with corresponding value
+  top3_25th <- ifel( rast(trip_data[percentile=="25th", paths]) == 1,
+                     trip_data[percentile=="25th", revenue_per_cell_75], 0)
+  top3_50th <- ifel( rast(trip_data[percentile=="50th", paths]) == 1,
+                     trip_data[percentile=="50th", revenue_per_cell_75], 0)
+  top3_75th <- ifel( rast(trip_data[percentile=="75th", paths]) == 1,
+                     trip_data[percentile=="75th", revenue_per_cell_75], 0)
+  cumulative_top3 <- mosaic(top3_75th, top3_50th, fun = "sum")
+  cumulative_top3 <- mosaic(cumulative_top3, top3_25th, fun = "sum")
+  
+  # Repeat for top 2 confidence levels with corresponding value
+  top2_25th <- ifel( rast(trip_data[percentile=="25th", paths]) == 1,
+                     trip_data[percentile=="25th", revenue_per_cell_50], 0)
+  top2_50th <- ifel( rast(trip_data[percentile=="50th", paths]) == 1,
+                     trip_data[percentile=="50th", revenue_per_cell_50], 0)
+  cumulative_top2 <- mosaic(top2_50th, top2_25th, fun = "sum")
+  
+  # Repeat for top 1 confidence levels with corresponding value
+  top1_25th <- ifel( rast(trip_data[percentile=="25th", paths]) == 1,
+                     trip_data[percentile=="25th", revenue_per_cell_25], 0)
+  cumulative_top1 <- top1_25th
+  
   # create file names and write cumulative rasters
-  path_base <- paste0(dir_output, "/vtrbs_cumulative_imgid")
+  path_base <- paste0(dir_output, "/vtrbs_cumulative_revenue_imgid")
+ 
+  path_top4 <- paste0(path_base, "/top4_", this_trip,".tif")
+  writeRaster(cumulative_top4, path_top4, overwrite=TRUE)
   
-  path_25 <- paste0(path_base, "/25thPercentile_", this_trip,".tif")
-  writeRaster(cumulative_25, path_25, overwrite=TRUE)
+  path_top3 <- paste0(path_base, "/top3_", this_trip,".tif")
+  writeRaster(cumulative_top3, path_top3, overwrite=TRUE)
   
-  path_50 <- paste0(path_base, "/50thPercentile_", this_trip,".tif")
-  writeRaster(cumulative_50, path_50, overwrite=TRUE)
+  path_top2 <- paste0(path_base, "/top2_", this_trip,".tif")
+  writeRaster(cumulative_top2, path_top2, overwrite=TRUE)
   
-  path_75 <- paste0(path_base, "/75thPercentile_", this_trip,".tif")
-  writeRaster(cumulative_75, path_75, overwrite=TRUE)
+  path_top1 <- paste0(path_base, "/top1_", this_trip,".tif")
+  writeRaster(cumulative_top1, path_top1, overwrite=TRUE)
   
-  path_100 <- paste0(path_base, "/100thPercentile_", this_trip,".tif")
-  writeRaster(cumulative_100, path_100, overwrite=TRUE)
-  
-  this_trip_id <- unique(dt_paths_vtrb_split[trip_area == this_trip,tripid])
-  this_imgid <- unique(dt_paths_vtrb_split[trip_area == this_trip,imgid])
-  
-  
-  these_paths <- data.table(tripid =c(this_trip_id, this_trip_id, this_trip_id, this_trip_id),
-                            trip_area= c(this_trip, this_trip, this_trip, this_trip),
-                            imgid = c(this_imgid, this_imgid, this_imgid, this_imgid),
-                            percentile=c("25th", "50th", "75th", "100th"),
-                            paths = c(path_25,path_50, path_75,path_100))
+  these_paths <- data.table(imgid =c(this_trip, this_trip, this_trip, this_trip),
+                            confidence=c("top1", "top2", "top3", "top4"),
+                            paths = c(path_top1, path_top2, path_top3, path_top4))
   # Add file paths to data.table
-  dt_paths_vtrb_revenue <- rbindlist(list(dt_paths_vtrb_cumulative_imgid, these_paths))
+  dt_paths_vtrb_revenue <- rbindlist(list(dt_paths_vtrb_revenue, these_paths))
 }
 
 saveRDS(dt_paths_vtrb_revenue,
