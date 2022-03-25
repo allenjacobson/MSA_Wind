7382a4b2ebca7a2e963f0928fbdda49fe323b46c
Lianne Allen-Jacobson <lianne.allen-jacobson@noaa.gov>
2022-03-25 12:07:37 -0400

    fixed labels to use 90th instead of 100th percentile following coveration with Geret

--- 2b_split_rasters_by_imgid_percentile.R
+++ 2b_split_rasters_by_imgid_percentile.R
@@ -29,7 +29,7 @@ dt_paths_vtrb <- readRDS(paste0(dir_output, "/dt_paths_vtrb.rds"))
 # create empty data table to add summary data
 dt_paths_vtrb_split <- data.table()
 unique_imgids <- dt_paths_vtrb$imgid #identify unique imgids for loop
-#this_imgid <- unique_imgids[[1]]
+this_imgid <- unique_imgids[[1]]
 
 for (this_imgid in unique_imgids) {
   this_tripid <- dt_paths_vtrb[imgid==this_imgid,tripid] # find tripID for imgid
@@ -48,13 +48,13 @@ for (this_imgid in unique_imgids) {
       # add column identifyiny value by its percentile - assumes that percentiles can be ranked
       # this could be wrong if there are large chunks missing from some percentiles but not others
       # however, a quick look shows the same colors for each buffer, suggesting they can be ranked
-      unique_values[, percentile := c("25th", "50th", "75th", "100th", "outsideBuffer")]
+      unique_values[, percentile := c("25th", "50th", "75th", "90th", "outsideBuffer")]
       
       # select parts of raster equal to unique values
       raster_25<- this_rast == unique_values[percentile=="25th", values]
       raster_50<- this_rast == unique_values[percentile=="50th", values]
       raster_75<- this_rast == unique_values[percentile=="75th", values]
-      raster_100<- this_rast == unique_values[percentile=="100th", values]
+      raster_90<- this_rast == unique_values[percentile=="90th", values]
       
       # Count the number of cells with each value
       count_25 <- global(this_rast== unique_values[percentile=="25th", values],
@@ -63,10 +63,10 @@ for (this_imgid in unique_imgids) {
                          sum, na.rm=TRUE)
       count_75 <- global(this_rast== unique_values[percentile=="75th", values],
                          sum, na.rm=TRUE)
-      count_100 <- global(this_rast== unique_values[percentile=="100th", values],
+      count_90 <- global(this_rast== unique_values[percentile=="90th", values],
                          sum, na.rm=TRUE)
       
-      counts <-rbind(count_25, count_50, count_75, count_100)
+      counts <-rbind(count_25, count_50, count_75, count_90)
       counts <- data.table(sum = counts$sum)
       
       # create file names, save rasters
@@ -80,17 +80,17 @@ for (this_imgid in unique_imgids) {
       new_path_75 <-  paste0(path_base, "/75th_", this_tripid, "_", this_imgid, ".tif")
       writeRaster(raster_75, new_path_75, overwrite=TRUE)
       
-      new_path_100 <-  paste0(path_base, "/100th_", this_tripid, "_", this_imgid, ".tif")
-      writeRaster(raster_100, new_path_100, overwrite=TRUE)
+      new_path_90 <-  paste0(path_base, "/90th_", this_tripid, "_", this_imgid, ".tif")
+      writeRaster(raster_90, new_path_90, overwrite=TRUE)
       
       # combine tripID, imgid, percentile, and path into a data table
       these_paths <- data.table(tripid= c(this_tripid, this_tripid, this_tripid, this_tripid),
                                 trip_area = c(this_trip_area, this_trip_area, this_trip_area, this_trip_area),
                              imgid=c(this_imgid, this_imgid, this_imgid, this_imgid),
-                             percentile=c("25th", "50th", "75th", "100th"),
+                             percentile=c("25th", "50th", "75th", "90th"),
                              value = unique_values[1:4, values],
                              counts = counts[,sum],
-                             paths = c(new_path_25,new_path_50, new_path_75,new_path_100))
+                             paths = c(new_path_25,new_path_50, new_path_75,new_path_90))
       
       dt_paths_vtrb_split <- rbindlist(list(dt_paths_vtrb_split, these_paths))
       
@@ -104,8 +104,8 @@ for (this_imgid in unique_imgids) {
     }
 }
 
-dt_paths_vtrb_split[, value_lower_3 := ifelse(percentile == "100th", 0, (1/3)/counts)][
-  , value_lower_2 := ifelse(percentile %in% c("100th", "75th"), 0, (1/2)/counts)][
-    , value_lower_1 := ifelse(percentile %in% c("100th", "75th", "50th"), 0, (1/1)/counts)]
+dt_paths_vtrb_split[, value_lower_3 := ifelse(percentile == "90th", 0, (1/3)/counts)][
+  , value_lower_2 := ifelse(percentile %in% c("90th", "75th"), 0, (1/2)/counts)][
+    , value_lower_1 := ifelse(percentile %in% c("90th", "75th", "50th"), 0, (1/1)/counts)]
 
 saveRDS(dt_paths_vtrb_split, paste0(dir_output, "/dt_paths_vtrb_split_by_percentile.rds"))
